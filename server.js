a2e7e176c4d376f707a8f5f410466376d7500c84
JosipRoncevic <josiproncevic8@gmail.com>
2025-05-29 12:35:10 +0000

    random commit

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+
 var http = require('http')
+var os = require('os') // <-- dodano
 
 var port = 8080
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Hello World\n')
+  response.end('!!!!!Hostname: ' + os.hostname() + '\n') // <-- promjena ovdje
 })
 
 server.listen(port)
 
 console.log('Server running at http://localhost:' + port)
+
