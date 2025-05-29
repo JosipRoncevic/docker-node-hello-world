94bcf6b8ad0d4de857cf1b6111fa8efb49add766
JosipRoncevic <120661404+JosipRoncevic@users.noreply.github.com>
2025-05-29 15:04:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var port = 8080
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('!!!!!Hostname: ' + os.hostname() + '\n') // <-- promjena ovdje
+  response.end('Hostname of a host is: ' + os.hostname() + '\n') // <-- promjena ovdje
 })
 
 server.listen(port)
